b0dcb8046373b8417c778e100e4cdc688083003d
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-08-24 03:30:23 +0700

    move auth form

---
+++ components/molecules/AuthForm.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+interface loginForm {
+  email: string
+  password: string
+}
+
+const data = reactive<loginForm>({
+  email: '',
+  password: ''
+})
+
+const handleLogin = () => {
+  console.log(JSON.stringify(data))
+}
+</script>
+
+<template>
+  <div>
+    <div class="pt-5 space-y-6">
+      <AtomicUIInput
+        v-model="data.email"
+        label="Email address"
+        type="email"
+        placeholder="your email"
+      />
+      <AtomicUIInput
+        v-model="data.password"
+        label="Passwords"
+        type="password"
+        placeholder="********"
+      />
+
+      <div>
+        <button @click="handleLogin">
+          Submit
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
