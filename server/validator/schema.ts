d8cc8f12f3e812915690d50fffe9e00a42257352
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-08-24 04:53:49 +0700

    add login schema

--- server/validator/schema.ts
+++ server/validator/schema.ts
@@ -16,3 +16,15 @@ export const userRegisterSchema = z.object({
   })
     .min(6, 'Password must be at least 6 characters long')
 })
+
+export const userLoginSchema = z.object({
+  email: z.string({
+    required_error: 'Email is required'
+  })
+    .email('Invalid email address'),
+
+  password: z.string({
+    required_error: 'Password is required'
+  })
+    .min(6, 'Password must be at least 6 characters long')
+})
