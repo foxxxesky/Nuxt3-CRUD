e1ba8470d5f3aa06a9849ef16a60abd35b046498
Syariif Abdurrahman Bathik <abdurrahman1467@gmail.com>
2023-08-24 04:53:41 +0700

    create jwt

---
+++ server/utils/jwt.ts
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/default */
+
+import jwt from 'jsonwebtoken'
+import { userData } from '~/types'
+
+const generateAccessToken = (user: userData) => {
+  const config = useRuntimeConfig()
+
+  return jwt.sign({
+    userId: user.id,
+    email: user.email
+  }, config.jwtAccessTokenSecret, {
+    expiresIn: '15m'
+  })
+}
+
+const generateRefreshToken = (user: userData) => {
+  const config = useRuntimeConfig()
+
+  return jwt.sign({
+    userId: user.id,
+    email: user.email
+  }, config.jwtRefreshTokenSecret, {
+    expiresIn: '6h'
+  })
+}
+
+export const generateTokens = (user: userData) => {
+  const accessToken = generateAccessToken(user)
+  const refreshToken = generateRefreshToken(user)
+
+  return {
+    accessToken,
+    refreshToken
+  }
+}
+
+export const sendRefreshToken = (event, token) => {
+  setCookie(event, 'refresh_token', token, {
+    httpOnly: true,
+    sameSite: true
+  })
+}
